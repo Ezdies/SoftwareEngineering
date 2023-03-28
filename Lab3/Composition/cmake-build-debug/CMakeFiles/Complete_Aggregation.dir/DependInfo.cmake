4139767623cbdd4bd6789e9b1e9ae5d31b85b655
Ezdies <bvbclubbvb@gmail.com>
2023-03-28 11:16:41 +0200

    Added composition example

---
+++ Lab3/Composition/cmake-build-debug/CMakeFiles/Complete_Aggregation.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/bvbcl/Documents/Inżynieria oprogramowania/Lab3/Complete Aggregation/THeart.cpp" "CMakeFiles/Complete_Aggregation.dir/THeart.cpp.o" "gcc" "CMakeFiles/Complete_Aggregation.dir/THeart.cpp.o.d"
+  "/mnt/c/Users/bvbcl/Documents/Inżynieria oprogramowania/Lab3/Complete Aggregation/THuman.cpp" "CMakeFiles/Complete_Aggregation.dir/THuman.cpp.o" "gcc" "CMakeFiles/Complete_Aggregation.dir/THuman.cpp.o.d"
+  "/mnt/c/Users/bvbcl/Documents/Inżynieria oprogramowania/Lab3/Complete Aggregation/main.cpp" "CMakeFiles/Complete_Aggregation.dir/main.cpp.o" "gcc" "CMakeFiles/Complete_Aggregation.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab3/Composition/cmake-build-debug/CMakeFiles/Complete_Aggregation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Complete_Aggregation.dir/THeart.cpp.o"
+  "CMakeFiles/Complete_Aggregation.dir/THeart.cpp.o.d"
+  "CMakeFiles/Complete_Aggregation.dir/THuman.cpp.o"
+  "CMakeFiles/Complete_Aggregation.dir/THuman.cpp.o.d"
+  "CMakeFiles/Complete_Aggregation.dir/main.cpp.o"
+  "CMakeFiles/Complete_Aggregation.dir/main.cpp.o.d"
+  "Complete_Aggregation"
+  "Complete_Aggregation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Complete_Aggregation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
